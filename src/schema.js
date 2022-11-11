1bbdd339e122edb5e1bb190a72c99eabad09bcf3
Michael Vecchione <mikevec23@gmail.com>
2022-11-11 15:27:09 -0500

    Fixing spelling mistake

--- src/schema.js
+++ src/schema.js
@@ -36,7 +36,7 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int @depricated(reason: "Use durationInSeconds")
+    length: Int @deprecated(reason: "Use durationInSeconds")
     durationInSeconds: Int
     "The number of modules this track contains"
     modulesCount: Int
@@ -63,7 +63,7 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int @depricated(reason: "Use durationInSeconds")
+    length: Int @deprecated(reason: "Use durationInSeconds")
     durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
